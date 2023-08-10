651db89a561313030821fcad946f32be6ba359be
gyutaepark <gyutaepark96+github@gmail.com>
2023-08-10 16:59:22 -0400

    Suppressed repeated warning message

--- read_inputs.py
+++ read_inputs.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import warnings
 
 def read_inputs():
+    warnings.simplefilter(action='ignore', category=UserWarning)
     required_inputs = pd.read_excel("MITHEx_inputs.xlsx", sheet_name="Plant Description", index_col=0, header=None)
     optional_inputs = pd.read_excel("MITHEx_inputs.xlsx", sheet_name="HX Parameters", index_col=0, header=None)
     cycle_inputs = pd.read_excel("MITHEx_inputs.xlsx", sheet_name="Cycle Parameters", index_col=0, header=None)
